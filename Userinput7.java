04cb65ee235975af83051466fe9f9a4fb10e3519
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-03-03 19:27:10 -0600

    Add files via upload

---
+++ Userinput7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Userinput7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter pokemon 1:");
+        String pokemon2 = scan.nextLine();
+
+        System.out.println("Enter pokemon 1:");
+        String pokemon3 = scan.nextLine();
+
+        System.out.println("Enter pokemon 1:");
+        String pokemon4 = scan.nextLine();
+
+        System.out.println("Enter pokemon 1:");
+        String pokemon5 = scan.nextLine();
+
+        System.out.println("Enter pokemon 1:");
+        String pokemon6 = scan.nextLine();
+
+        System.out.println("Here are your pokemon");
+        System.out.println(pokemon2);
+        System.out.println(pokemon3);
+        System.out.println(pokemon4);
+        System.out.println(pokemon5);
+        System.out.println(pokemon6);
+    }
+}
