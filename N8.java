8dfd29bf7759d7a42aa7017563780998957bc283
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-03-05 15:07:43 -0600

    Add files via upload

---
+++ N8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class N8 {
+    public static void main(String[] args){
+        Scanner Boo = new Scanner(System.in);
+
+        System.out.println("How was your day? ");
+        String day = Boo.nextLine();
+        System.out.println("Did you like it?");
+        String like = Boo.nextLine();
+        System.out.println("Seems like it was a lovely day");
+        String dayy = Boo.nextLine();
+        System.out.println("You are done !!!");
+
+    }
+}
