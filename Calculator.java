f1ca9cf3386aad31a83d9a653c5069ee00121862
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-03-09 13:48:34 -0500

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,32 @@
+package org.example;
+import static org.junit.jupiter.api.Assertions.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Calculator {
-    int a;
-    int b;
 
-    public Calculator(int a, int b){
-    this.a = a;
-    this.b = b;
+public char determineLetterGrade(int numberGrade){
+    if(numberGrade < 0) {
+    throw new IllegalArgumentException("NumberGrade cannot be negative");
+
+    }  else if(numberGrade < 50){
+        return 'B';
+
+    } else if(numberGrade < 70){
+        return 'C';
+
+    } else if(numberGrade < 80){
+        return 'D';
+    } else if(numberGrade < 90){
+        return 'E';
+    } else {
+        return 'A';
     }
 
+}
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
